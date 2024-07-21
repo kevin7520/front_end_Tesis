86a1cdbbf2fd6bef1637e40d39da602decc6f9cc
kevin7520 <kevin7520@hotmail.com>
2024-07-21 16:51:30 -0500

    Cambios

--- src/app/modules/proformas_module/view/proformas/proformas.component.ts
+++ src/app/modules/proformas_module/view/proformas/proformas.component.ts
@@ -3,6 +3,10 @@ import { proformaData } from '../../models/Response/obtenerProformasResponse';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProformaService } from '../../services/proforma.service';
 import { MatPaginator } from '@angular/material/paginator';
+import { ServiciosService } from 'src/app/modules/servicios_module/services/servicios.service';
+import { Respuestos } from 'src/app/modules/servicios_module/models/respuestos.model';
+import { NotificationService } from 'src/app/shared/services/notificacion.service';
+import { detalleProformna } from '../../models/Response/obtenerRespuestoResponse';
 
 @Component({
   selector: 'app-proformas',
@@ -11,13 +15,95 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class ProformasComponent implements OnInit, AfterViewInit {
 
-  constructor(private proformas : ProformaService) { }
+  constructor(private proformas: ProformaService, private _ServiciosService: ServiciosService, private notificationService: NotificationService) { }
+  
+  proformaCrearItems: any[] = [];
+  proformaData!: proformaData;  
+  idRepuestoElegido: number = 0;
+  cantidad: number = 0;
+  paso = 1;
   ngOnInit() {
-    //this.obtenerProformas();
+    this.obtenerProformas();
+    //this.cargarRespuesto();
   }
 
   ngAfterViewInit(): void {
-    //this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
+
+  verProforma: boolean = false;
+  id_proforma = 0;
+  displayedColumns: string[] = 
+  [
+    'idProforma', 
+    'nombre', 
+    'identificacion', 
+    'subtotal', 
+    'iva',
+    'total',
+    'acciones'
+    ];
+  
+  PROFORMA_DATA: proformaData[] = [];
+  dataSource = new MatTableDataSource<proformaData>(this.PROFORMA_DATA);
+ @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  
+
+
+  obtenerProformas()
+  {
+    this.proformas.getDatos().subscribe(dataResponse => {
+      if (dataResponse.codigo == "200") {
+        this.PROFORMA_DATA = dataResponse.data;
+        this.dataSource = new MatTableDataSource<proformaData>(this.PROFORMA_DATA);
+        this.dataSource.paginator = this.paginator;
+      }
+    })
+  }
+  
+
+  editarProforma(id: number) {
+    this.proformaData = this.PROFORMA_DATA.find(item => item.idProforma == id)!;
+    this.paso = 3;
+
+  }
+
+  verProformaMetodo(id: number) {
+    this.id_proforma = id;
+    this.verProforma = true;
+  }
+
+  descargarProformaMetodo(id: number) {
+    this.id_proforma = id;
+  }
+
+  nombre = "";
+  identificacion = "";
+
+   filtrar() {
+    let dataTemp = [...this.PROFORMA_DATA];
+    if (this.nombre != "") {
+      dataTemp = dataTemp.filter(data=> data.cliente.nombres.toString().toLowerCase().includes(this.nombre) )
+    }
+    if (this.identificacion != "") {
+      dataTemp = dataTemp.filter(data=> data.cliente.cedula.toLowerCase().includes(this.identificacion) )
+    }
+    this.dataSource = new MatTableDataSource<proformaData>(dataTemp);
+    this.dataSource.paginator = this.paginator;
+   }
+  
+  borrarFiltro() {
+    this.nombre = "";
+    this.identificacion = "";
+    this.dataSource = new MatTableDataSource<proformaData>(this.PROFORMA_DATA);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  creacionEdicionProforma() {
+    this.obtenerProformas();
+    this.paso = 1;
+  }
+
   
 }
